f8ed0dd411f278f762b5f868cab49f46f0e6f022
omkar2809 <55548995+omkar2809@users.noreply.github.com>
2020-05-01 12:55:38 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import {baseURL} from '../shared/baseURL';
 
 
 const required = (val) => val && val.length;
@@ -105,7 +105,7 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
     return (
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImg width="100%" src={baseURL + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import {baseURL} from '../shared/baseURL';
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -13,7 +14,7 @@ function RenderCard({ item, isLoading, errMess }) {
     }
     else return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseURL + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 { item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
@@ -31,7 +32,8 @@ function Home(props) {
                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion}
+                    isLoading={props.promosLoading} errMess={props.promosErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
